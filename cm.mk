5d2a08c598cddd203f8f857af54525c1f789d497
OmarEinea <eineao@gmail.com>
2014-08-25 10:08:54 +0400

    togari: update signature (.133)
    Signed-off-by: Omar Einea <eineao@gmail.com>
    Change-Id: I34ceafe984673d6be6c18301ef99c2a6faa0aec4

--- cm.mk
+++ cm.mk
@@ -6,7 +6,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 $(call inherit-product, device/sony/togari/full_togari.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6833 BUILD_FINGERPRINT=Sony/C6833/C6833:4.3/14.2.A.0.290/eng.hudsonslave:user/release-keys PRIVATE_BUILD_DESC="C6833-user 4.3 RHINE-1.1-131125-1201 eng.hudsonslave test-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6833 BUILD_FINGERPRINT=Sony/C6833/C6833:4.4.4/14.4.A.0.133/ev__jQ:user/release-keys PRIVATE_BUILD_DESC="C6833-user 4.4.4 14.4.A.0.133 ev__jQ release-keys"
 
 PRODUCT_NAME := cm_togari
 PRODUCT_DEVICE := togari

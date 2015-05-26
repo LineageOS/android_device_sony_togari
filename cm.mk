e75f72178d67fd2edbd8fbf694c571f3f5f7a273
David Viteri <davidteri91@gmail.com>
2015-05-26 21:41:51 +0200

    togari: update fingerprint to latest lollipop release
    Change-Id: I714c35637d2b11421e5a40f6ee1489bac55664a8
    Signed-off-by: David Viteri <davidteri91@gmail.com>

--- cm.mk
+++ cm.mk
@@ -6,7 +6,9 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 $(call inherit-product, device/sony/togari/full_togari.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6833 BUILD_FINGERPRINT=Sony/C6833/C6833:4.4.4/14.4.A.0.133/ev__jQ:user/release-keys PRIVATE_BUILD_DESC="C6833-user 4.4.4 14.4.A.0.133 ev__jQ release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6833
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Sony/C6833/C6833:5.0.2/14.5.A.0.270/3305956307:user/release-keys
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="C6833-user 5.0.2 14.5.A.0.270 3305956307 release-keys"
 
 PRODUCT_NAME := cm_togari
 PRODUCT_DEVICE := togari
